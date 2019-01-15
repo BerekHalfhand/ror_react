071f7232a53d7df4dac332196c171e916947ff48
Moorindal <moorindal@ymail.com>
2019-01-15 15:38:41 -0500

    init

---
+++ app/javascript/packs/rorApp/components/landingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class LandingPage extends React.Component {
+  render() {
+    return(
+      <div>
+        <h1 className="hello">Hello World</h1>
+      </div>
+    )
+  }
+}
+
+export default LandingPage
