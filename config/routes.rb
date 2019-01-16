70869200ef229afe4f5a3f7e3ba5f9186f8c320f
Moorindal <moorindal@ymail.com>
2019-01-16 15:07:30 -0500

    Routing fix

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "pages#root"
+  get '/userList', to: "pages#root"
 
   namespace :api do
     namespace :v1 do
       resources :user, only: [:index, :create, :destroy, :update]
     end
   end
+
+  get '*path' => redirect('/')  
 end
